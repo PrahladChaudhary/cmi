4545faa5cbbf50e2811d58c12fce1970d59878dc
PrahladChaudhary <prahlad.singh@impressico.com>
2022-11-25 14:15:45 +0530

    cmi testing code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardModule } from './../admin/dashboard/dashboard.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./../admin/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./../login/login.module').then(m => m.LoginModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), DashboardModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    userpwd: new FormControl('', Validators.required),
+  });
+
+  errorMessage:string = '';
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  login(): void {
+    this.errorMessage = '';
+    if(this.isValidUser()) this.router.navigate(['/dashboard']);
+  }
+
+  isValidUser(): boolean {
+    if(!this.loginForm.valid || this.loginForm.controls['username'].value !== 'admin' || 
+      this.loginForm.controls['userpwd'].value !== 'super') {
+        this.errorMessage = 'Please enter the correct username and password';
+        this.loginForm.reset();
+        return false
+      }
+      return true;
+  }
+
+}
